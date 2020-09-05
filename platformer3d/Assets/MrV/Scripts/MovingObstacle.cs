8c44d60658be0fa47559eb343bc79f0d6c3efafa
Tiger-The-Cat <70044734+Tiger-The-Cat@users.noreply.github.com>
2020-09-05 10:20:49 -0700

    What a Treasure!
    Added two colors, a little bit of a platformer, a moving block, and made the lighting better

---
+++ platformer3d/Assets/MrV/Scripts/MovingObstacle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    public float moveTime = 2.5f;
+    public float moveAmount = 0;
+    public float moveSpeed = 5;
+    public Vector3 direction = Vector3.forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        moveAmount += Time.deltaTime;
+        if (moveAmount >= moveTime)
+        {
+            moveAmount = 0;
+            if (direction == Vector3.forward)
+            {
+                direction = Vector3.back;
+            }
+            else
+            {
+                if (direction == Vector3.back)
+                {
+                    direction = Vector3.forward;
+                }
+            }
+            if (direction == Vector3.left)
+            {
+                direction = Vector3.right;
+            }
+            else
+            {
+                if (direction == Vector3.right)
+                {
+                    direction = Vector3.left;
+                }
+            }
+                if (direction == Vector3.up)
+                {
+                    direction = Vector3.down;
+                }
+            else
+            {
+                if (direction == Vector3.down)
+                {
+                    direction = Vector3.up;
+                }
+            }
+        }
+    }
+}
